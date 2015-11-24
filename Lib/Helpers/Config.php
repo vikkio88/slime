600168af4dce627d865e7194a00dcae6da5496df
vincenzo.ciaccio <vincenzo.ciaccio@timesofmalta.com>
2015-11-24 12:18:30 +0100

    updated config get

--- Lib/Helpers/Config.php
+++ Lib/Helpers/Config.php
@@ -15,17 +15,18 @@ namespace App\Lib\Helpers;
  */
 class Config
 {
-
     /**
      * @param $key
+     * @param null $directory
      * @return null
      */
-    public static function get($key)
+    public static function get($key, $directory = null)
     {
+        $fileName = $directory . "config/";
+        var_dump($fileName);
         $exp = explode(".", $key);
-        if (is_array($exp) && $exp[0] != null && $exp[1] != null && file_exists("config/" . $exp[0] . ".php"))
-        {
-            $conf = include("config/" . $exp[0] . ".php");
+        if (is_array($exp) && $exp[0] != null && $exp[1] != null && file_exists($fileName . $exp[0] . ".php")) {
+            $conf = include($fileName . $exp[0] . ".php");
             $val = array_key_exists($exp[1], $conf) ? $conf[$exp[1]] : null;
             return $val;
         }
