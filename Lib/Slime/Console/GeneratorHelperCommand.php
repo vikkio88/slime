06174833b7252ece05fc6fb906b14434c5c372a6
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2016-10-18 22:52:39 +0100

    created generator command iface added create config command

---
+++ Lib/Slime/Console/GeneratorHelperCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Lib\Slime\Console;
+
+
+abstract class GeneratorHelperCommand extends SlimeCommand
+{
+    public function run()
+    {
+        $result = file_put_contents(
+            $this->getFilePath() . $this->getFileName() . $this->getFileExtension()
+            ,
+            $this->getHead() . $this->getStub()
+        );
+        return $result !== false ? 0 : 1;
+    }
+
+    protected abstract function getFilePath();
+
+    protected function getHead()
+    {
+        return "<?php" . PHP_EOL;
+    }
+
+    protected function getFileName()
+    {
+        return $this->getArg(0);
+    }
+
+    protected function getFileExtension()
+    {
+        return ".php";
+    }
+
+    protected function getStub()
+    {
+        return "";
+    }
+
+}
