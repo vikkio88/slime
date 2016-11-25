d6767606158485dc397ad20c857ec58b5d812ea5
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2016-11-25 22:40:33 +0000

    imported user token model , migration and seeder

---
+++ database/migrations/M1478637077UserTokens.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use App\Lib\Slime\Interfaces\DatabaseHelpers\DbHelperInterface;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use \Illuminate\Database\Schema\Blueprint as Blueprint;
+
+class M1478637077UserTokens implements DbHelperInterface
+{
+
+    public function run()
+    {
+        $tableName = 'user_tokens';
+        Capsule::schema()->dropIfExists($tableName);
+        Capsule::schema()->create($tableName, function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index()->unsigned();
+            $table->string('user_ip')->nullable();
+            $table->string('device_token')->nullable();
+            $table->string('device_name')->nullable();
+            $table->string('token');
+            $table->dateTime('last_usage');
+            $table->timestamps();
+
+            $table->unique('token');
+        });
+    }
+
+}
---
+++ database/seeds/S1478638829UserTokens.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Lib\Slime\Interfaces\DatabaseHelpers\DbHelperInterface;
+use App\Models\Users\User;
+use App\Models\Users\UserToken;
+
+class S1478638829UserTokens implements DbHelperInterface
+{
+
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $users = User::all();
+        foreach ($users as $user) {
+            UserToken::create(
+                [
+                    'user_id' => $user->id,
+                    'user_ip' => $faker->ipv4,
+                    'device_token' => $faker->password,
+                    'device_name' => $faker->colorName,
+                    'token' => str_repeat($user->id, 6),
+                    'last_usage' => \Carbon\Carbon::now(),
+                ]
+            );
+        }
+    }
+
+}
